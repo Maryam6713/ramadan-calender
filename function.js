e5eeddbde12a71314b6dc274748eb5295c1c23d0
Maryam6713 <maryammalik6713@gmail.com>
2024-07-17 23:09:24 +0500

    initial commit

--- function.js
+++ function.js
@@ -9,24 +9,24 @@ setInterval(function(){
 },500)
 
 
-var ramadanDate = new Date('February 28, 2025 00:00:00').getTime();
+var remainingTime = new Date('February 28, 2025 00:00:00').getTime();
 
 setInterval(function() {
     var now = new Date().getTime();
-    var distance = ramadanDate - now;
+    var ramadan = remainingTime - now;
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var days = Math.floor(ramadan/ (1000 * 60 * 60 * 24));
+    var hours = Math.floor((ramadan % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((ramadan % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((ramadan % (1000 * 60)) / 1000);
 
     document.getElementById('days').innerText = days;
     document.getElementById('hours').innerText = hours;
     document.getElementById('minutes').innerText = minutes;
     document.getElementById('seconds').innerText = seconds;
 
-    if (distance < 0) {
+    if (ramadan < 0) {
         clearInterval(intervalId);
-        document.getElementById('countdown').innerHTML = "Ramadan has started!";
+        document.getElementById('time').innerHTML = "Rmadan Mubarak to all Muslims!";
     }
 }, 1000);
